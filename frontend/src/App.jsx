2e795f74f92c27856551d541b1b0db00be5c5d7b
Bharat <bharat.c2753@gmail.com>
2024-07-25 16:38:36 +0530

    created the home page to show all books from the database

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
     return (
@@ -8,7 +14,6 @@ const App = () => {
             <Route path="/books/details/:id" element={<ShowBook />} />
             <Route path="/books/edit/:id" element={<EditBook />} />
             <Route path="/books/delete/:id" element={<DeleteBook />} />
-            {/* <div className="bg-red-400 text-white">App</div> */}
         </Routes>
     );
 };
