e395b33c7c2a2ce0e8f85c96ca8361e005240e1f
Bharat Choudhary <125469847+bharat-c27@users.noreply.github.com>
2024-12-10 13:48:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,10 +28,7 @@ app.use(express.json());
 // const cors = require('cors');
 
 app.use(cors({
-  origin: [
-    'https://book-hub-frontend-two.vercel.app',  
-    'http://localhost:3000'  
-  ],
+  origin: "*",
   credentials: true,  
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
